51826f51c295ae5023c8158b24cbc694d8b9a8d2
Liang Cheng <liang.cheng@gmail.com>
2014-10-16 07:01:00 +0000

    add my_blog plugin

---
+++ php/mod/my_blog/start.php
@@ -0,0 +1,13 @@
+<?php
+
+elgg_register_action("my_blog/save", elgg_get_plugins_path() . "my_blog/actions/my_blog/save.php");
+
+elgg_register_page_handler('my_blog', 'my_blog_page_handler');
+
+function my_blog_page_handler($segments) {
+    if ($segments[0] == 'add') {
+        include elgg_get_plugins_path() . 'my_blog/pages/my_blog/add.php';
+        return true;
+    }
+    return false;
+}
