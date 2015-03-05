c373b7c5878af79e63396c93058902026966ebe3
Liang Cheng <liang.cheng@gmail.com>
2015-03-05 02:46:19 +0000

    revert previous commit

--- php/mod/web_services/lib/user.php
+++ php/mod/web_services/lib/user.php
@@ -1848,7 +1848,7 @@ expose_function('user.set_seller',
                 true,
                 true);
 
-
+/*
 function user_set_points($username, $points) {
     $user = get_user_by_username($username);
  
@@ -1873,3 +1873,4 @@ expose_function('user.set_points',
                 'POST',
                 true,
                 true);
+*/
