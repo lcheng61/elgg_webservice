d792384be69ca9df03fc43426fd6d0890bbff15c
Liang Cheng <liang.cheng@gmail.com>
2015-03-27 04:50:47 +0000

    removed points += 0

--- php/mod/web_services/lib/ideas.php
+++ php/mod/web_services/lib/ideas.php
@@ -428,7 +428,7 @@ function ideas_post_tip($message, $idea_id)
             if (!$user->points) {
                 $user->points = 0; // signup points
             }
-            $user->points += 0;
+//            $user->points = $user->points + 0;
             add_to_river('river/object/market/create','create', $user->guid, $post->guid);
         }
     } else {
