6d3659ea638b15899e2cef8f19b0f77a747e1a27
Liang Cheng <liang.cheng@gmail.com>
2015-05-03 06:35:52 +0000

    change 404 page to homepage, not showing elgg

--- php/views/default/errors/404.php
+++ php/views/default/errors/404.php
@@ -3,6 +3,9 @@
  * Page not found error
  */
 
-$message = elgg_echo('error:404');
+//$message = elgg_echo('error:404');
 
-echo "<h2>$message</h2>";
+//echo "<h2>$message</h2>";
+
+header('Location: http://m.lovebeauty.me');
+exit;
