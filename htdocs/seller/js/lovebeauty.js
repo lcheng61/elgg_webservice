12429e47e0dfa7875472c3873aeef416e94299fc
LiuBin <liubin@daydaydev.com>
2015-08-09 22:51:53 -0700

    Add preview function for local files.

--- htdocs/seller/js/lovebeauty.js
+++ htdocs/seller/js/lovebeauty.js
@@ -1,11 +1,11 @@
 //product server
-var server = 'http://m.lovebeauty.me/';
-var api_key = '87573c9e87172e86b8a3e99bd73f1d9e9c19086b';
+//var server = 'http://m.lovebeauty.me/';
+//var api_key = '87573c9e87172e86b8a3e99bd73f1d9e9c19086b';
 
 //development server
-//var server = 'http://social.routzi.com/'
+var server = 'http://social.routzi.com/'
 //var server = 'http://www.lovebeauty.me/'
-//var api_key = 'badb0afa36f54d2159e599a348886a7178b98533';
+var api_key = 'badb0afa36f54d2159e599a348886a7178b98533';
 
 
 //dev server
@@ -49,6 +49,7 @@ var check_user_email_availability = 'services/api/rest/json/?method=user.check_e
 var statistics = 'services/api/rest/json/?method=payment.analyze.seller_order';
 var user_get_settings = 'services/api/rest/json/?method=user.get_seller_setting';
 var user_set_settings = 'services/api/rest/json/?method=user.set_seller_setting';
+is_user_login = 'services/api/rest/json/?method=site.test_auth';
 
 var username;
 var token;
@@ -97,6 +98,12 @@ $(function() {
 	if (window.location.href.indexOf("signup.html") < 0 && window.location.href.indexOf("login.html") < 0 && window.location.href.indexOf("reset_password.html") < 0 && (username == undefined || username == "")) {
 		window.location.href = "login.html"
 	}
+
+	isUserLogin(function(isLogin) {
+		if (window.location.href.indexOf("signup.html") < 0 && window.location.href.indexOf("login.html") < 0 && window.location.href.indexOf("reset_password.html") < 0 && !isLogin) {
+			window.location.href = "login.html"
+		}
+	});
 
 })
 
@@ -163,4 +170,19 @@ function getUrlParameter(sParam) {
 			return sParameterName[1];
 		}
 	}
+}
+
+
+function isUserLogin(callback) {
+	var get_user_auth_url = server + is_user_login + '&api_key=' + api_key + '&auth_token=' + getCookie('token');
+
+	$.getJSON(get_user_auth_url, function(data) {
+		console.log(JSON.stringify(data));
+		if (data.status == 0) { //read prodcut detail successfully.
+
+			callback(true);
+		} else {
+			callback(false);
+		}
+	});
 }
